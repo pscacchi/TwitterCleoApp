40ce41bb001fe32d3b0351669b9569e8f4f49160
PScacchi <pabloantonio-scacchi@endava.com>
2022-06-16 15:40:42 -0300

    TCA-25 Some useless line were removed

--- app/src/main/java/ar/scacchipa/twittercloneapp/viewmodel/MainAppViewModel.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/viewmodel/MainAppViewModel.kt
@@ -3,15 +3,11 @@ package ar.scacchipa.twittercloneapp.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.preference.PreferenceManager
 import ar.scacchipa.twittercloneapp.domain.StartModeController
 import ar.scacchipa.twittercloneapp.ui.Route
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainAppViewModel(app: Application): AndroidViewModel( app ) {
-
-    val sharedPreference = PreferenceManager.getDefaultSharedPreferences( app )
-    var splashWasShowed = sharedPreference.getBoolean("splashWasShowed", false)
 
     var currentPage: MutableStateFlow<String> = MutableStateFlow( Route.SplashScreen.route )
 
@@ -20,6 +16,5 @@ class MainAppViewModel(app: Application): AndroidViewModel( app ) {
         scope = viewModelScope,
         splashCallback = { currentPage.value = Route.SplashScreen.route },
         loginCallback = { currentPage.value = Route.LoginScreen.route}
-    )
 
 }
