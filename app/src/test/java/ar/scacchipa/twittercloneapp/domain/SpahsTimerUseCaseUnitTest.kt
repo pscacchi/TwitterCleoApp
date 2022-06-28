827c29f932259b4e46e939b6e6e3d45c9b073aca
PScacchi <pabloantonio-scacchi@endava.com>
2022-06-28 14:57:08 -0300

    TCA-28 runCurrent and advanceUntilIdle were deleted

--- app/src/test/java/ar/scacchipa/twittercloneapp/domain/SpahsTimerUseCaseUnitTest.kt
+++ app/src/test/java/ar/scacchipa/twittercloneapp/domain/SpahsTimerUseCaseUnitTest.kt
@@ -1,8 +1,6 @@
 package ar.scacchipa.twittercloneapp.domain
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -14,15 +12,12 @@ class SpahsTimerUseCaseUnitTest {
         var wasSpent = false
         val timer = SplashTimerUseCase()
         assertEquals(false, wasSpent)
-        this.runCurrent()
         val initialTime = this.testScheduler.currentTime
         wasSpent = timer.spendSplash()
 
         val changeTime = this.testScheduler.currentTime
 
         assertEquals(5000, changeTime - initialTime)
-        assertEquals(true, wasSpent)
-        this.advanceUntilIdle()
         assertEquals(true, wasSpent)
     }
 }
