11ccc2c79dd2ea66d03bbe2d351a84c11229be97
PScacchi <pabloantonio-scacchi@endava.com>
2022-06-27 23:15:39 -0300

    TCA-28 SplashScreen UI test was implemented

--- app/src/androidTest/java/ar/scacchipa/twittercloneapp/ui/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ar/scacchipa/twittercloneapp/ui/ExampleInstrumentedTest.kt
@@ -1,24 +1,42 @@
 package ar.scacchipa.twittercloneapp.ui
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import ar.scacchipa.twittercloneapp.R
+import ar.scacchipa.twittercloneapp.component.TwitterCloneActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@LargeTest
+class SplashTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(TwitterCloneActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ar.scacchipa.twittercloneapp", appContext.packageName)
+    fun splashSpendInFiveSeconds() {
+        onView(withId(R.id.twitter_logo_imageview))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.layout.fragment_login_layout))
+            .check(doesNotExist())
+
+        Thread.sleep(4000)
+
+        onView(withId(R.id.twitter_logo_imageview))
+            .check(matches(isDisplayed()))
+
+        Thread.sleep(1000)
+
+        onView(withId(R.id.twitter_logo_imageview))
+            .check(doesNotExist())
     }
 }
