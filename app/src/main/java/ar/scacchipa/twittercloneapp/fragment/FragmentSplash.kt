bc18269b3ea158757d7e5d984aaf959893d51c5a
PScacchi <pabloantonio-scacchi@endava.com>
2022-06-29 22:20:02 -0300

    TCA-30 The statusbar and navigation bar colors was fix

--- app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentSplash.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentSplash.kt
@@ -1,12 +1,12 @@
 package ar.scacchipa.twittercloneapp.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import ar.scacchipa.twittercloneapp.R
 import ar.scacchipa.twittercloneapp.databinding.FragmentSplashLayoutBinding
@@ -27,6 +27,16 @@ class FragmentSplash: Fragment() {
             if (splashWasSpent) {
                 findNavController().navigate(R.id.action_fragmentSplash_to_fragmentLogin)
             }
+        }
+        activity?.window?.let { window ->
+            window.statusBarColor = Color.TRANSPARENT
+            window.decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            )
+            window.navigationBarColor =
+                requireContext().resources.getColor(R.color.primary_background)
         }
         splashViewModel.spendSplash()
         return binding?.root
