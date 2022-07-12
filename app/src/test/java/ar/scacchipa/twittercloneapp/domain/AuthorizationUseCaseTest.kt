df3c1252de047e4bfb87e3fd0a569f9b748ac59f
PScacchi <pabloantonio-scacchi@endava.com>
2022-07-11 21:08:23 -0300

    TCA-32 the changes requested in the PR were corrected

--- app/src/test/java/ar/scacchipa/twittercloneapp/domain/AuthorizationUseCaseTest.kt
+++ app/src/test/java/ar/scacchipa/twittercloneapp/domain/AuthorizationUseCaseTest.kt
@@ -10,7 +10,7 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class AuthorizationUseCaseTest {
 
-    val authorizationUseCase = AuthorizationUseCase(MockAuthorizationRespository())
+    private val authorizationUseCase = AuthorizationUseCase(MockAuthorizationRespository())
 
     @Test
     fun authorizationUseCaseShouldReturnAUserAccessToken() = runTest {
@@ -26,7 +26,6 @@ class AuthorizationUseCaseTest {
                 errorDescription = ""
             ))
     }
-
 }
 
 class MockAuthorizationRespository: IAuthorizationRepository {
