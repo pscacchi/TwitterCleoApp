a4d868da832ebd148a4eec00c9c70f81d7a7e4a6
PScacchi <pabloantonio-scacchi@endava.com>
2022-07-14 14:23:38 -0300

    TCA-34 Unnecesary class were deleted

--- app/src/main/java/ar/scacchipa/twittercloneapp/domain/NavToLoginWithoutMsgUseCase.kt
+++
@@ -1,9 +0,0 @@
-package ar.scacchipa.twittercloneapp.domain
-
-import ar.scacchipa.twittercloneapp.datasource.UserAccessToken
-
-class NavToLoginWithoutMsgUseCase {
-    operator fun invoke(): UserAccessToken {
-        return UserAccessToken(error = "")
-    }
-}
