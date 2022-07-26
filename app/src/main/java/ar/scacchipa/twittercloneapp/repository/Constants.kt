d84059fe1f59975b09354a89bf40b207b72ad243
PScacchi <pabloantonio-scacchi@endava.com>
2022-07-26 17:46:58 -0300

    TCA-49 The test of constant value was added

--- app/src/main/java/ar/scacchipa/twittercloneapp/repository/Constants.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/repository/Constants.kt
@@ -1,30 +1,32 @@
 package ar.scacchipa.twittercloneapp.repository
 
-object Constants {
-    const val CLIENT_ID = "Yzg1a01Hcm16RTdKdmptZmhJdEs6MTpjaQ"
-    const val REDIRECT_URI = "https://twittercloneendava.firebaseapp.com/__/auth/handler"
+class Constants {
+    companion object {
+        const val CLIENT_ID = "Yzg1a01Hcm16RTdKdmptZmhJdEs6MTpjaQ"
+        const val REDIRECT_URI = "https://twittercloneendava.firebaseapp.com/__/auth/handler"
 
-    const val RESPONSE_TYPE_CODE = "code"
-    const val STATE_STATE = "state"
-    const val CODE_CHALLENGE_CHALLENGE = "challenge"
-    const val CODE_CHALLENGE_METHOD_PLAIN = "plain"
+        const val RESPONSE_TYPE_CODE = "code"
+        const val STATE_STATE = "state"
+        const val CODE_CHALLENGE_CHALLENGE = "challenge"
+        const val CODE_CHALLENGE_METHOD_PLAIN = "plain"
 
-    const val GRANT_TYPE_AUTHORIZATION_CODE = "authorization_code"
-    const val CODE_VERIFIER_CHALLENGE = "challenge"
+        const val GRANT_TYPE_AUTHORIZATION_CODE = "authorization_code"
+        const val CODE_VERIFIER_CHALLENGE = "challenge"
 
-    const val SERVER_PARAMETER_CODE = "code"
-    const val SERVER_PARAMETER_ERROR = "error"
+        const val SERVER_PARAMETER_CODE = "code"
+        const val SERVER_PARAMETER_ERROR = "error"
 
-    const val ERROR_HOST_LOOKUP_TOKEN = "error_host_lookup"
-    const val ERROR_ACCESS_DENIED = "access_denied"
+        const val ERROR_HOST_LOOKUP_TOKEN = "error_host_lookup"
+        const val ERROR_ACCESS_DENIED = "access_denied"
 
-    const val SCOPE_USERS_READ = "users.read"
-    const val SCOPE_TWEET_READ = "tweet.read"
-    const val SCOPE_TWEET_WRITE = "tweet.write"
-    const val SCOPE_OFFLINE_ACCESS = "offline.access"
-    const val SCOPE_LIST_READ = "list.read"
-    const val SCOPE_FOLLOWS_READ = "follows.read"
-    const val SCOPE_LIKE_READ = "like.read"
-    const val SCOPE_LIKE_WRITE = "like.write"
-    const val SCOPE_SPACE_READ = "space.read"
+        const val SCOPE_USERS_READ = "users.read"
+        const val SCOPE_TWEET_READ = "tweet.read"
+        const val SCOPE_TWEET_WRITE = "tweet.write"
+        const val SCOPE_OFFLINE_ACCESS = "offline.access"
+        const val SCOPE_LIST_READ = "list.read"
+        const val SCOPE_FOLLOWS_READ = "follows.read"
+        const val SCOPE_LIKE_READ = "like.read"
+        const val SCOPE_LIKE_WRITE = "like.write"
+        const val SCOPE_SPACE_READ = "space.read"
+    }
 }
---
+++ app/src/test/java/ar/scacchipa/twittercloneapp/repository/ConstantsTest.kt
@@ -0,0 +1,43 @@
+package ar.scacchipa.twittercloneapp.repository
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ConstantsTest {
+
+    @Test
+    fun constant() {
+        assertEquals("Yzg1a01Hcm16RTdKdmptZmhJdEs6MTpjaQ", Constants.CLIENT_ID,)
+        assertEquals("https://twittercloneendava.firebaseapp.com/__/auth/handler", Constants.REDIRECT_URI,)
+
+        assertEquals("code", Constants.RESPONSE_TYPE_CODE)
+        assertEquals("state", Constants.STATE_STATE)
+        assertEquals("challenge", Constants.CODE_CHALLENGE_CHALLENGE)
+        assertEquals("plain", Constants.CODE_CHALLENGE_METHOD_PLAIN)
+
+        assertEquals("authorization_code", Constants.GRANT_TYPE_AUTHORIZATION_CODE)
+        assertEquals("challenge", Constants.CODE_VERIFIER_CHALLENGE)
+
+        assertEquals("code", Constants.SERVER_PARAMETER_CODE)
+        assertEquals("error", Constants.SERVER_PARAMETER_ERROR)
+
+        assertEquals("error_host_lookup", Constants.ERROR_HOST_LOOKUP_TOKEN)
+        assertEquals("access_denied", Constants.ERROR_ACCESS_DENIED)
+
+        assertEquals("users.read", Constants.SCOPE_USERS_READ)
+        assertEquals("tweet.read", Constants.SCOPE_TWEET_READ)
+        assertEquals("tweet.write", Constants.SCOPE_TWEET_WRITE)
+        assertEquals("offline.access", Constants.SCOPE_OFFLINE_ACCESS)
+        assertEquals("list.read", Constants.SCOPE_LIST_READ)
+        assertEquals("follows.read", Constants.SCOPE_FOLLOWS_READ)
+        assertEquals("like.read", Constants.SCOPE_LIKE_READ)
+        assertEquals("like.write", Constants.SCOPE_LIKE_WRITE)
+        assertEquals("space.read", Constants.SCOPE_SPACE_READ)
+    }
+
+    @Test
+    fun compObj() {
+        val c = Constants()
+        assertEquals(true, c is Constants)
+    }
+}
