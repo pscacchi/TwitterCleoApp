f0251e0a0887dea6bf925365cacf45f0da857b9b
PScacchi <pabloantonio-scacchi@endava.com>
2022-08-05 01:02:24 -0300

    TCA-40 The draft skeleton navigation design was created

--- app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentHome.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentHome.kt
@@ -6,23 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import ar.scacchipa.twittercloneapp.databinding.FragmentLoginSuccessBinding
+import ar.scacchipa.twittercloneapp.databinding.FragmentHomeBinding
 
 class FragmentHome : Fragment() {
 
     private val args: FragmentHomeArgs by navArgs()
-    private var binding: FragmentLoginSuccessBinding? = null
+    private var binding: FragmentHomeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginSuccessBinding.inflate(inflater)
+        binding = FragmentHomeBinding.inflate(inflater)
+        binding?.homeBottomNavigationView?.itemIconTintList = null
+        binding?.homeBottomNavigationView?.itemActiveIndicatorColor = null
         return binding?.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding?.bearerCode?.text = args.bearerToken
     }
 
     override fun onDestroyView() {
