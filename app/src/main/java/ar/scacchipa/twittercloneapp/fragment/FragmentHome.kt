95ce0555359b57c992fbafdfaadac5cc0fd8cde6
PScacchi <pabloantonio-scacchi@endava.com>
2022-08-05 10:08:35 -0300

    TCA-40 some value were extrated to de library and the statusBarColor and the navigationBarColor were changed to white

--- app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentHome.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/fragment/FragmentHome.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import ar.scacchipa.twittercloneapp.R
 import ar.scacchipa.twittercloneapp.databinding.FragmentHomeBinding
 
 class FragmentHome : Fragment() {
@@ -17,6 +19,11 @@ class FragmentHome : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        activity?.window?.let { window ->
+            window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+            window.navigationBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+        }
         binding = FragmentHomeBinding.inflate(inflater)
         binding?.homeBottomNavigationView?.itemIconTintList = null
         binding?.homeBottomNavigationView?.itemActiveIndicatorColor = null
