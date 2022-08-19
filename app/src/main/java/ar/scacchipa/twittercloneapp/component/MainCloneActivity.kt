eae6c0514cfab578d52cf6966a3dcd455d9526e0
PScacchi <pabloantonio-scacchi@endava.com>
2022-08-18 22:35:34 -0300

    TCA-40 The statusbar and navigation settings were moved to a theme

--- app/src/main/java/ar/scacchipa/twittercloneapp/component/MainCloneActivity.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/component/MainCloneActivity.kt
@@ -1,10 +1,8 @@
 package ar.scacchipa.twittercloneapp.component
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -45,13 +43,6 @@ class MainCloneActivity: AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
 
-        this.window?.apply {
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE or
-                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            statusBarColor = ContextCompat.getColor(this@MainCloneActivity, R.color.white)
-            navigationBarColor = ContextCompat.getColor(this@MainCloneActivity, R.color.on_primary)
-        }
         this.supportActionBar?.apply {
             displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
             elevation = 0f
