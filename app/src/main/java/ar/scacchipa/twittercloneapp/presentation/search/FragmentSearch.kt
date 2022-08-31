869285b035136ec9713724bf5727d1677654c99f
PScacchi <pabloantonio-scacchi@endava.com>
2022-08-31 09:11:36 -0300

    TCA-55 Now, the statusBar don't change background color when it navegates to the FragmentSearch

--- app/src/main/java/ar/scacchipa/twittercloneapp/presentation/search/FragmentSearch.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/presentation/search/FragmentSearch.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import ar.scacchipa.twittercloneapp.R
 import ar.scacchipa.twittercloneapp.databinding.FragmentSearchBinding
@@ -19,10 +18,6 @@ class FragmentSearch : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        activity?.window?.let { window ->
-            window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
-            window.navigationBarColor = ContextCompat.getColor(requireContext(), R.color.white)
-        }
         binding = FragmentSearchBinding.inflate(inflater)
 
         (activity as AppCompatActivity).supportActionBar
