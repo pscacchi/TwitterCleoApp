01b07f3f3c1ac18684f9c5b84abd4c877228e991
PScacchi <pabloantonio-scacchi@endava.com>
2022-09-09 08:22:30 -0300

    TCA-52 he getConsumableAuthCode() and getScope() functions were moved to LoginWebSectiobViewModel class.

--- app/src/main/java/ar/scacchipa/twittercloneapp/presentation/login/FragmentLoginWebSection.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/presentation/login/FragmentLoginWebSection.kt
@@ -87,7 +87,7 @@ class FragmentLoginWebSection : Fragment() {
                 }
             }
 
-            loadUrl(getConsumableAuthCode())
+            loadUrl(viewModel.getConsumableAuthCode())
         }
         return binding?.root
     }
@@ -96,31 +96,4 @@ class FragmentLoginWebSection : Fragment() {
         super.onDestroyView()
         binding = null
     }
-
-    private fun getConsumableAuthCode(): String {
-        return "https://twitter.com/i/oauth2/authorize?" +
-                "response_type=${Constants.RESPONSE_TYPE_CODE}&" +
-                "client_id=${Constants.CLIENT_ID}&" +
-                "redirect_uri=${Constants.REDIRECT_URI}&" +
-                "scope=${getScope()}&" +
-                "state=${Constants.STATE_STATE}&" +
-                "code_challenge=${Constants.CODE_CHALLENGE_CHALLENGE}&" +
-                "code_challenge_method=${Constants.CODE_CHALLENGE_METHOD_PLAIN}"
-    }
-
-    private fun getScope(): String {
-        return listOf(
-            Constants.SCOPE_USERS_READ,
-            Constants.SCOPE_TWEET_READ,
-            Constants.SCOPE_TWEET_WRITE,
-            Constants.SCOPE_OFFLINE_ACCESS,
-            Constants.SCOPE_LIST_READ,
-            Constants.SCOPE_FOLLOWS_READ,
-            Constants.SCOPE_LIKE_READ,
-            Constants.SCOPE_LIKE_WRITE,
-            Constants.SCOPE_SPACE_READ
-        ).joinToString("%20")
-    }
-
-
 }
