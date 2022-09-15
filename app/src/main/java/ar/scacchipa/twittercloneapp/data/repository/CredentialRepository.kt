7f921b17088bc6c8850aa393c7bd17c297cfd77c
PScacchi <pabloantonio-scacchi@endava.com>
2022-09-15 14:23:49 -0300

    TCA-54 Proof rebase

--- app/src/main/java/ar/scacchipa/twittercloneapp/data/repository/CredentialRepository.kt
+++ app/src/main/java/ar/scacchipa/twittercloneapp/data/repository/CredentialRepository.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.withContext
 open class CredentialRepository(
     private val credentialLocalSource: ILocalSource,
     private val accessTokenExternalSource: IAuthDataSource,
+    private val variable: Boolean,
     private val mapper: IMapper<UserAccessToken, Credential>,
     private val dispatcherDefault: CoroutineDispatcher = Dispatchers.Default,
     private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
